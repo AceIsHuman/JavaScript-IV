d567dc782eeb32df678de81c3d2c3b8fdb3cc3d9
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-05-29 23:08:12 -0500

    created person class and added objects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,7 +2,13 @@
 
 class Person {
     constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+    }
 
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am grom ${this.location}.`)
     }
 }
 
@@ -23,6 +29,14 @@ class Students extends Person {
 class ProjectManagers extends Instructors {
     constructor(attributes) {
         super(attributes);
-        
+
     }
 }
+
+const george = new Person({name: 'George', age: 28, location: 'San Antonio'});
+const oscar = new Person({name: 'Oscar', age: 23, location: 'Austin'});
+const matt = new Person({name: 'Matt', age: 27, location: 'Del Rio'})
+
+console.log(george);
+console.log(oscar);
+console.log(matt);
