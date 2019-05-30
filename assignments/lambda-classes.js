687c9f23308b2f92d812c3888e53f9b4d31778ac
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-05-29 23:01:32 -0500

    set up structure for classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,28 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attributes) {
+
+    }
+}
+
+class Instructors extends Person {
+    constructor(attributes) {
+        super(attributes);
+
+    }
+}
+
+class Students extends Person {
+    constructor(attributes) {
+        super(attributes);
+
+    }
+}
+
+class ProjectManagers extends Instructors {
+    constructor(attributes) {
+        super(attributes);
+        
+    }
+}
