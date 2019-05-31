c03de1cae8464894a1d172119f34a302c99bd15e
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-05-30 20:47:08 -0500

    added Instructor class and objects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,35 +8,56 @@ class Person {
     }
 
     speak() {
-        console.log(`Hello my name is ${this.name}, I am grom ${this.location}.`)
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
     }
 }
 
-class Instructors extends Person {
+class Instructor extends Person {
+    constructor(attributes) {
+        super(attributes);
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}.`);
+    }
+
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}.`);
+    }
+}
+
+class Student extends Person {
     constructor(attributes) {
         super(attributes);
 
     }
 }
 
-class Students extends Person {
+class ProjectManager extends Instructor {
     constructor(attributes) {
         super(attributes);
 
     }
 }
 
-class ProjectManagers extends Instructors {
-    constructor(attributes) {
-        super(attributes);
-
-    }
-}
+// Person Class Objects
 
 const george = new Person({name: 'George', age: 28, location: 'San Antonio'});
 const oscar = new Person({name: 'Oscar', age: 23, location: 'Austin'});
 const matt = new Person({name: 'Matt', age: 27, location: 'Del Rio'})
 
 console.log(george);
-console.log(oscar);
-console.log(matt);
+console.log(oscar.speak());
+console.log(matt.speak());
+
+// Instuctor Class Objects
+
+const danFrehner = new Instructor({name: `Dan Frehner`, age: 36, location: `California`, favLanguage: `Javascript`, specialty: `Playing Banjo`, catchPhrase: `Well, let's try it!`});
+const danLevy = new Instructor({name: `Dan Levy`, age: 32, location: `California`, favLanguage: `HTML`, specialty: `Bananas`, catchPhras: `It's just a banana word!`});
+
+console.log(danLevy.speak());
+console.log(danFrehner.grade(george, `JavaScript III`));
+console.log(danLevy.demo(`CSS Compilers`));
